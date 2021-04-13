6ba0b32c34e0ef5f8942060fca0e9ecbcc30324e
SlmSlm <solomonkoviktor67@gmail.com>
2021-04-13 17:23:07 +0300

    Update day-03.js

--- day-03.js
+++ day-03.js
@@ -7,3 +7,47 @@ return "false";
 return "true";
 }
 }
+
+// http://www.codewars.com/kata/57202aefe8d6c514300001fd
+
+function saleHotdogs(n) {
+  if (n < 5) {
+    return n * 100;
+  } else if (n >= 5 && n < 10) {
+    return n * 95;
+  } else {
+    return n * 90;
+  }
+}
+
+// http://www.codewars.com/kata/572059afc2f4612825000d8a
+
+function howManydays(month) {
+  var days;
+  switch (month) {
+    case 2:
+      days = 28;
+      break;
+
+    case 4:
+      days = 30;
+      break;
+
+    case 6:
+      days = 30;
+      break;
+
+    case 9:
+      days = 30;
+      break;
+
+    case 11:
+      days = 30;
+      break;
+
+    default:
+      days = 31;
+      break;
+  }
+  return days;
+}

43bba01cd88e10b418740c560ca11674d62f3e1d
SlmSlm <solomonkoviktor67@gmail.com>
2021-04-16 17:34:34 +0300

    Update day-05.js

--- day-05.js
+++ day-05.js
@@ -8,3 +8,25 @@ function correctTail(bod, tail) {
     return false;
   }
 }
+
+// https://www.codewars.com/kata/56f6ad906b88de513f000d96
+
+function bonusTime(salary, bonus) {
+  if (bonus == true) {
+    return "\u00A3" + salary * 10;
+  } else {
+    return "\u00A3" + salary;
+  }
+}
+
+//https://www.codewars.com/kata/5a58d889880385c2f40000aa
+
+function automorphic(n) {
+  let square = Math.pow(n, 2).toString();
+  let num = n.toString();
+  if (square.substring(square.length - num.length) == n) {
+    return "Automorphic";
+  } else {
+    return "Not!!";
+  }
+}

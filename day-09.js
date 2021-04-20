10b18b013a935264116c27ebd808dc9afe103088
SlmSlm <solomonkoviktor67@gmail.com>
2021-04-20 23:21:20 +0300

    Update day-09.js

--- day-09.js
+++ day-09.js
@@ -12,3 +12,26 @@ function cutIt(arr) {
   }
   return newArr;
 }
+
+// http://www.codewars.com/kata/57277a31e5e51450a4000010
+
+function firstToLast(str, c) {
+  const firstPos = str.indexOf(c);
+  const lastPos = str.lastIndexOf(c);
+  if (firstPos === -1) {
+    return -1;
+  }
+  return lastPos - firstPos;
+}
+
+// http://www.codewars.com/kata/57280481e8118511f7000ffa
+
+const words = string.split(" ");
+console.log(words);
+for (let i = 0; i < words.length; i++) {
+  if (words[i] != " ") {
+    words[i] = words[i].split("").join(separator);
+  }
+  console.log(words);
+}
+return words.join(" ");

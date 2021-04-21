5c2c4e95a78f536c71af6ba390486c397c006b42
SlmSlm <solomonkoviktor67@gmail.com>
2021-04-21 23:11:55 +0300

    Update day-10.js

--- day-10.js
+++ day-10.js
@@ -13,8 +13,34 @@ function alienLanguage(str) {
   return result;
 }
 
-// 
+// http://www.codewars.com/kata/57284d23e81185ae6200162a
 
+function topSecret(str) {
+  const words = str.split("");
+  let res = "";
+  for (let i = 0; i < str.length; i++) {
+    let decryption = str[i].charCodeAt() - 3;
+    if (str[i].charCodeAt() === 32 || str[i].charCodeAt() === 33) {
+      res += String.fromCharCode(str[i].charCodeAt());
+    } else if (str[i].charCodeAt() === 98) {
+      res += String.fromCharCode(121);
+    } else if (str[i].charCodeAt() === 97) {
+      res += String.fromCharCode(120);
+    } else {
+      res += String.fromCharCode(decryption);
+    }
+  }
+  return res.toString();
+}
 
+// хардкодінг наше все :)
 
-//
+// http://www.codewars.com/kata/5729b103dd8bac11a900119e
+
+function fiveLine(s) {
+  let words = "";
+  for (let i = 0; i <= 5; i++) {
+    words += `${s.trim().repeat(i)}\n`;
+  }
+  return words.trim();
+}

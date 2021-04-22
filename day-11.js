c418d37a01ab1bd2e6a4f994c48b1537f5b2113e
SlmSlm <solomonkoviktor67@gmail.com>
2021-04-22 22:13:37 +0300

    Update day-11.js

--- day-11.js
+++ day-11.js
@@ -1,1 +1,36 @@
+// https://www.codewars.com/kata/playing-with-sets-equal-or-not/train/javascript
 
+function areEqual(s1, s2) {
+  for (let item of s1) {
+    if (!s2.has(item)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function notEqual(s1, s2) {
+  for (let item of s1) {
+    if (!s2.has(item)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// https://www.codewars.com/kata/5609fd5b44e602b2ff00003a/train/javascript
+
+function process2Arrays(arr1, arr2) {
+  let amountOfEls = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.indexOf(arr1[i]) != -1) {
+      amountOfEls++;
+    }
+  }
+  for (let j = 0; j < arr2.length; j++) {}
+  let amountOfArr1Els = arr1.length - amountOfEls;
+  let amountOfArr2Els = arr2.length - amountOfEls;
+  let numOfEl = amountOfArr1Els + amountOfArr2Els;
+  let result = [amountOfEls, numOfEl, amountOfArr1Els, amountOfArr2Els];
+  return result;
+}

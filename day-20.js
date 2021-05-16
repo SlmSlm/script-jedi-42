5ec1dc85dd1306bcf32fbafb15331d64938a564b
SlmSlm <solomonkoviktor67@gmail.com>
2021-05-16 14:29:15 +0300

    Create day-20.js

---
+++ day-20.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/573bca07dffc1aa693000139
+
+const regex=/^(-?)9.*0000$/;
+
+// http://www.codewars.com/kata/573d11c48b97c0ad970002d4
+
+const regex = /htt(p:|ps:)\/\/[a-z\d.]+.(com|net)/ig;

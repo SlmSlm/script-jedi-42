74cdb97a219951f487635750640994e5a985ce53
SlmSlm <solomonkoviktor67@gmail.com>
2021-05-16 14:32:22 +0300

    Update day-20.js

--- day-20.js
+++ day-20.js
@@ -4,4 +4,4 @@ const regex=/^(-?)9.*0000$/;
 
 // http://www.codewars.com/kata/573d11c48b97c0ad970002d4
 
-const regex = /htt(p:|ps:)\/\/[a-z\d.]+.(com|net)/ig;
+const regex = /htt(p:|ps:)\/\/[a-z\d.]+\.(com|net)/ig;

c1d8209a2feb921db23213feda2566170fad5a86
SlmSlm <solomonkoviktor67@gmail.com>
2021-05-19 14:24:39 +0300

    Create day-21.js

---
+++ day-21.js
@@ -0,0 +1,10 @@
+// http://www.codewars.com/kata/573e6831e3201f6a9b000971
+
+const regex= /\b(\w)(\w)?(\w)?\w?\3\2\1\b/g;
+
+// http://www.codewars.com/kata/573fb9223f9793e485000453
+
+const regex = /(\d)(?=(\d{3})+$)/g;
+function addCommas(money, reg) {
+  return money.replace(reg, (x) => x + ",");
+}

7254cb8508da97c553e8826c92fa37b46e81279f
SlmSlm <solomonkoviktor67@gmail.com>
2021-05-25 20:54:53 +0300

    Update day-28.js

--- day-28.js
+++ day-28.js
@@ -5,3 +5,13 @@ function always(n) {
     return n;
   };
 }
+
+// https://www.codewars.com/kata/59bdbe9d46038724ca0000b9/train/javascript
+
+function solution(arr, options) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; ++i) {
+    newArray.push(arr[i] + 2 * options.modifier);
+  }
+  return newArray;
+}
